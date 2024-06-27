dd536ae612fb4cf202c34e1c654c68c401249e68
Haritha R <harithalaks1998@gmail.com>
2024-06-27 12:50:20 +0530

    Add project files.

---
+++ sample task/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 20;
+            int B = 5;
+            int C;
+            C=A+B;
+            Console.WriteLine("Addition value is :{0}",C);
+            C = A - B;
+            Console.WriteLine("subtraction value is :{0}", C);
+            C = A * B;
+            Console.WriteLine("Multiplication value is :{0}", C);
+            C = A / B;
+            Console.WriteLine("Division value is :{0}", C);
+            C = A % B;
+            Console.WriteLine("Division value is :{0}", C);
+            Console.ReadLine();
+
+
+        }
+    }
+}
